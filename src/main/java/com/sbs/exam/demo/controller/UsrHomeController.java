a68ce21295c5c3882729a062815e26bd8505c56c
scc2111 <scc2111@naver.com>
2021-09-27 12:05:45 +0900

    구현

---
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -0,0 +1,20 @@
+package com.sbs.exam.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbs.exam.demo.service.MemberService;
+
+@Controller
+public class UsrHomeController {
+	@RequestMapping("/usr/home/main")
+	public String showMain() {
+		return "usr/home/main";
+	}
+	
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
+	}
+}
---
+++ src/main/java/com/sbs/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -0,0 +1,25 @@
+package com.sbs.exam.demo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.sbs.exam.demo.vo.Rq;
+
+@Component
+public class BeforeActionInterceptor implements HandlerInterceptor {
+	private Rq rq;
+	
+	public BeforeActionInterceptor(Rq rq) {
+		this.rq = rq;
+	}
+	
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		rq.initOnBeforeActionInterceptor();
+		
+		return HandlerInterceptor.super.preHandle(req, resp, handler);
+	}
+}
